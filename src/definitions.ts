afa90885aaa8421529423076b588e8d68e83b8b2
Jordan Jensen <jensen.jordan08023@gmail.com>
2023-10-03 07:20:24 -0600

    It builds now

--- src/definitions.ts
+++ src/definitions.ts
@@ -33,9 +33,9 @@ declare module '@capacitor/cli' {
        *
        * @since 1.0.0
        * @default false
-       * @example "true"
+       * @example true
        */
-      LottieRemoteEnabled: boolean;
+      LottieRemoteEnabled?: boolean;
 
       /**
        * Location of the Lottie JSON file that should be loaded in light mode.
@@ -46,6 +46,15 @@ declare module '@capacitor/cli' {
        * @example "#FF9900"
        */
       LottieAnimationLocationLight?: string;
+
+      /**
+       * Android only! Scale type of the view. Can be one of the following: https://developer.android.com/reference/android/widget/ImageView.ScaleType
+       *
+       * @since 1.0.0
+       * @default FIT_CENTER
+       * @example "FIT_CENTER"
+       */
+      LottieScaleType?: string;
 
       /**
        * Location of the Lottie JSON file that should be loaded in dark mode.
@@ -81,9 +90,9 @@ declare module '@capacitor/cli' {
        *
        * @since 1.0.0
        * @default false
-       * @example "#FF9900"
+       * @example false
        */
-      LottieCancelOnTap: boolean;
+      LottieCancelOnTap?: boolean;
 
       /**
        * Duration after which the Lottie animation should be hidden.
@@ -91,7 +100,7 @@ declare module '@capacitor/cli' {
        *
        * @since 1.0.0
        * @default 0
-       * @example "#FF9900"
+       * @example 0.2
        */
       LottieHideTimeout?: number;
 
@@ -124,106 +133,96 @@ declare module '@capacitor/cli' {
        * @example "#FF9900"
        */
       LottieBackgroundColor?: string;
+
+      /**
+       * Width of the container that's rendering the Lottie animation
+       *
+       * @since 1.0.0
+       * @default 200
+       * @example 200
+       */
+      lottieWidth?: number;
+
+      /**
+       * Height of the container that's rendering the Lottie animation
+       *
+       * @since 1.0.0
+       * @default 200
+       * @example 200
+       */
+      LottieHeight?: number;
+
+      /**
+       * Uses width and height values as relative values. Specify them as e.g. 0.3 to have 30%.
+       *
+       * @since 1.0.0
+       * @default false
+       * @example true
+       */
+      LottieRelativeSize?: boolean;
+
+      /**
+       * Renders the animation in full screen. Ignores properties above.
+       * @since 1.0.0
+       * @default false
+       * @example true
+       */
+      LottieFullScreen?: boolean;
+
+      /**
+       * Loops the animation
+       *
+       * @since 1.0.0
+       * @default false
+       * @example true
+       */
+      LottieLoopAnimation?: boolean;
+
+      /**
+       * Hides the Lottie splash screen when the pageDidLoad event fired
+       *
+       * @since 1.0.0
+       * @default false
+       * @example true
+       */
+      LottieAutoHideSplashScreen?: boolean;
+
+      /**
+       * Android only! Enables hardware acceleration for the animation view.
+       * Not really recommended since Lottie decides automatically whether the hardware mode should be used or not.
+       *
+       * @since 1.0.0
+       * @default false
+       * @example true
+       */
+      LottieEnableHardwareAcceleration?: boolean;
+      /**
+       * Duration for the fade out animation.
+       * CAUTION: iOS reads this value in SECONDS, but e.g., 0.5 is supported. Android reads this value in MILLISECONDS! the Set to 0 disable the fade out animation.
+       *
+       * @since 1.0.0
+       * @default 0
+       * @example 0
+       */
+      LottieFadeOutDuration?: number;
+
+      /**
+       * Hides the Lottie splash screen after the animation has been played. Do not use together with LottieAutoHideSplashScreen or LottieLoopAnimation
+       *
+       * @since 1.0.0
+       * @default false
+       * @example true
+       */
+      LottieHideAfterAnimationEnd?: boolean;
+
+      /**
+       * Disables caching of animations
+       *
+       * @since 1.0.0
+       * @default false
+       * @example true
+       */
+      LottieCacheDisabled?: boolean;
     };
-
-    /**
-     * Width of the container that's rendering the Lottie animation
-     *
-     * @since 1.0.0
-     * @default 200
-     * @example 200
-     */
-    lottieWidth?: number;
-
-    /**
-     * Height of the container that's rendering the Lottie animation
-     *
-     * @since 1.0.0
-     * @default 200
-     * @example "200"
-     */
-    LottieHeight?: number;
-
-    /**
-     * Uses width and height values as relative values. Specify them as e.g. 0.3 to have 30%.
-     *
-     * @since 1.0.0
-     * @default false
-     * @example "true"
-     */
-    LottieRelativeSize: boolean;
-
-    /**
-     * Renders the animation in full screen. Ignores properties above.
-     * @since 1.0.0
-     * @default false
-     * @example "true"
-     */
-    LottieFullScreen: boolean;
-
-    /**
-     * Loops the animation
-     *
-     * @since 1.0.0
-     * @default false
-     * @example "true"
-     */
-    LottieLoopAnimation: boolean;
-
-    /**
-     * Hides the Lottie splash screen when the pageDidLoad event fired
-     *
-     * @since 1.0.0
-     * @default false
-     * @example true
-     */
-    LottieAutoHideSplashScreen: boolean;
-
-    /**
-     * Android only! Enables hardware acceleration for the animation view.
-     * Not really recommended since Lottie decides automatically whether the hardware mode should be used or not.
-     *
-     * @since 1.0.0
-     * @default false
-     * @example true
-     */
-    LottieEnableHardwareAcceleration: boolean;
-
-    /**
-     * Android only! Scale type of the view. Can be one of the following: https://developer.android.com/reference/android/widget/ImageView.ScaleType
-     *
-     * @since 1.0.0
-     * @default "FIT_CENTER"
-     * @example "FIT_CENTER"
-     */
-    LottieScaleType?: string;
-
-    /**
-     * Duration for the fade out animation.
-     * CAUTION: iOS reads this value in SECONDS, but e.g., 0.5 is supported. Android reads this value in MILLISECONDS! the Set to 0 disable the fade out animation.
-     *
-     * @since 1.0.0
-     * @default 0
-     * @example 0
-     */
-    LottieFadeOutDuration?: number;
-
-    /**
-     * Hides the Lottie splash screen after the animation has been played. Do not use together with LottieAutoHideSplashScreen or LottieLoopAnimation
-     *
-     * @since 1.0.0
-     * @default false
-     * @example true
-     */
-    LottieHideAfterAnimationEnd: boolean;
-
-    /**
-     * Disables caching of animations
-     *
-     * @since 1.0.0
-     * @default false
-     * @example true
-     */
-    LottieCacheDisabled: boolean;
   }
 }
